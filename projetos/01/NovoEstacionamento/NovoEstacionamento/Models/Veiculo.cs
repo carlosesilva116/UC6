a56f47b52de6e0ea261c1c1546c6fe98d278b93c
CARLOS EDUARDO DA SILVA <carlos.esilva116@senacsp.edu.br>
2025-10-07 19:47:30 -0300

    Atualização

--- projetos/01/NovoEstacionamento/NovoEstacionamento/Models/Veiculo.cs
+++ projetos/01/NovoEstacionamento/NovoEstacionamento/Models/Veiculo.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NovoEstacionamento.Models
+﻿namespace NovoEstacionamento.Models
 {
     internal class Veiculo
     {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
     }
 }
