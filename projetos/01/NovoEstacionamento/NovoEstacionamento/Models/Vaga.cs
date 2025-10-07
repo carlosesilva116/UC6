9ef005cc669f6099ab79ce8515234e38f1530f38
CARLOS EDUARDO DA SILVA <carlos.esilva116@senacsp.edu.br>
2025-10-06 22:10:47 -0300

    Aula01

---
+++ projetos/01/NovoEstacionamento/NovoEstacionamento/Models/Vaga.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoEstacionamento.Models
+{
+    internal class Vaga
+    {
+
+        public int ID { get; set; }
+
+        public string Localizacao { get; set; }
+
+        public string Tipo { get; set; }
+
+    }
+}
