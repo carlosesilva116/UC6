b49ca34ad2fbf96797507b1f46d5846f4ae8ae42
CARLOS EDUARDO DA SILVA <carlos.esilva116@senacsp.edu.br>
2025-10-13 22:15:56 -0300

    Entidade_Produtos

---
+++ projetos/02/CadastroDeUsuario/CadastroDeUsuario/Controller/ProdutoController.cs
@@ -0,0 +1,46 @@
+﻿using CadastroDeUsuario.Data;
+using CadastroDeUsuario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroDeUsuario.Controller
+{
+    internal class ProdutoController
+    {
+        private AppDbContext _context;
+        public ProdutoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AdicionarProduto()
+        {
+
+            Console.Clear();
+            #region Pedir_Dados
+            Console.Write("Nome do Produto: ");
+            string NomeProduto = Console.ReadLine();
+
+            Console.Write("Preço do Produto: ");
+            decimal PrecoProduto = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Data de Vencimento: ");
+            DateOnly VencimentoProduto = DateOnly.Parse(Console.ReadLine());
+
+            var novoProduto = new Models.Produto()
+            {
+                Nome = NomeProduto,
+                Preco = PrecoProduto,
+                Vencimento = VencimentoProduto
+            };
+
+            _context.Produtos.Add(novoProduto);
+            _context.SaveChanges();
+        }
+
+    }
+}
---
+++ projetos/02/CadastroDeUsuario/CadastroDeUsuario/Controller/UsuarioController.cs
@@ -0,0 +1,128 @@
+﻿using CadastroDeUsuario.Data;
+using CadastroDeUsuario.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace CadastroDeUsuario.Controller
+{
+    internal class UsuarioController
+    {
+        private AppDbContext _context;
+
+        public UsuarioController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Adicionar()
+        {
+            Console.Clear();
+            #region Pedir_Dados
+            Console.Write("Primeiro Nome: ");
+            string primeiroNome = Console.ReadLine();
+
+            Console.Write("Sobrenome: ");
+            string sobrenome = Console.ReadLine();
+
+            Console.Write("Data de Nascimento: ");
+            DateOnly nascimento = DateOnly.Parse(Console.ReadLine());
+            #endregion
+
+            var novoUsuario = new Usuario()
+            {
+                DataNascimento = nascimento,
+                PrimeiroNome = primeiroNome,
+                Sobrenome = sobrenome
+            };
+
+            _context.Usuarios.Add(novoUsuario);
+            _context.SaveChanges();
+
+            Console.WriteLine("Usuário Cadastrado");
+            Console.ReadKey();
+        }
+
+        public void Listar()
+        {
+            var usuarios = _context.Usuarios.ToList();
+
+            if (usuarios.Count() == 0)
+            {
+                Console.WriteLine("Nenum usuário cadastrado!");
+            }
+            else
+            {
+                foreach (var usuario in usuarios)
+                {
+                    Console.WriteLine($"ID: {usuario.Id} | Nome: {usuario.PrimeiroNome}");
+                }
+            }
+
+            Console.WriteLine("\nPressione qualquer telca para voltar.");
+            Console.ReadKey();
+        }
+
+   
+         public void Detalhes()
+        {
+            //Dizer onde estou
+            Console.Clear();
+            Console.WriteLine("=== Detalhes do Usuário ===");
+           
+            //Pedir o ID do usuario
+            Console.WriteLine("Digite o Id de usurio: ");
+            var idUsuario = int.Parse(Console.ReadLine());
+
+            //Buscar o usuario no banco de dados
+          var usuario = _context.Usuarios.FirstOrDefault(user =>user.Id == idUsuario);
+
+            //Se não encontrar, avisar o usuario
+            if (usuario == null)
+            {
+                Console.WriteLine("\nUsuário não encontrado!");
+                
+            }
+            //Se encontrar, mostrar os detalhes do usuário
+            else
+            {
+
+                Console.WriteLine(" --- Dados do Usuário --- ");
+                Console.WriteLine($"Id: {usuario.Id}");
+                Console.WriteLine ($"Nome: {usuario.PrimeiroNome}");
+                Console.WriteLine($"Sobrenome: {usuario.Sobrenome}");
+                Console.WriteLine($"Nascimento: {usuario.DataNascimento}");
+            }
+            Console.WriteLine("\nPressione qualquer tecla para voltar.");
+            Console.ReadKey();
+
+        }
+        public void Remover()
+        {
+            Console.Clear();
+            Console.WriteLine("=== Remover Usuário ===");
+            Console.Write("Digite o Id do usuário: ");
+            var idUsuario = int.Parse(Console.ReadLine());
+
+            // Buscar o usuário no banco de dados
+            var usuarioParaDeletar = _context.Usuarios
+                .FirstOrDefault(user => user.Id == idUsuario);
+
+            // Se não encontrar, avisar o usuário
+            if (usuarioParaDeletar == null)
+            {
+                Console.WriteLine("\nUsuário não encontrado!");
+                Console.ReadKey();
+                return;
+            }
+
+            // Se encontrar, deletar o usuário
+           _context.Usuarios.Remove(usuarioParaDeletar);
+            _context.SaveChanges();
+
+            Console.WriteLine("\nUsuário removido com sucesso!");
+            Console.ReadKey();
+
+
+        }
+    }
+}
