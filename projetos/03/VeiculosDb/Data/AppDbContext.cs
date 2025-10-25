eca065e6c67f4d05e53ac89cb79dc22be3b42553
CARLOS EDUARDO DA SILVA <carlos.esilva116@senacsp.edu.br>
2025-10-24 22:07:35 -0300

    Aula 04 Crud completo

---
+++ projetos/03/VeiculosDb/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeiculosDb.Models;
+
+namespace VeiculosDb.Data
+{
+    public class AppDbContext : DbContext
+    {
+     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
