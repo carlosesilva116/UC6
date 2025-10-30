80a9cac340909f23e1c7d94a1cd02f6538bc5158
CARLOS EDUARDO DA SILVA <carlos.esilva116@senacsp.edu.br>
2025-10-29 22:20:57 -0300

    AtualizacaodeTela

--- projetos/4/01/NovoEstacionamento/NovoEstacionamento/Models/Vaga.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NovoEstacionamento.Models
-{
-    internal class Vaga
-    {
-        public int Id { get; set; }
-        public string Localizacao { get; set; }
-        public string Tipo { get; set; }
-    }
-}
--- projetos/4/03/VeiculoApp/VeiculoApp/Models/Veiculo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VeiculoApp.Models
-{
-    internal class Veiculo
-    {
-        public int Id { get; set; }
-        public string? Marca { get; set; }
-        public string? Modelo { get; set; }
-    }
-}
