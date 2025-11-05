0c0fe621142b9495d3ed1773d06be9f1f558604b
CARLOS EDUARDO DA SILVA <carlos.esilva116@senacsp.edu.br>
2025-11-04 22:07:41 -0300

    CreateMotorista

--- UC6/04/EstacionamentoSenac/EstacionamentoSenac.API/Controllers/MotoristasController.cs
+++ UC6/04/EstacionamentoSenac/EstacionamentoSenac.API/Controllers/MotoristasController.cs
@@ -2,6 +2,7 @@
 using EstacionamentoSenac.API.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EstacionamentoSenac.API.Controllers
 {
@@ -19,14 +20,14 @@ namespace EstacionamentoSenac.API.Controllers
         [HttpGet]
         public ActionResult<List<Motorista>> GetMotoristas()
         {
-            return Ok(_context.Motoristas.ToList());
+            return Ok(_context.Motoristas.Include(m => m.Veiculo).ToList());
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Veiculo> GetMotoristaById(int id)
+        public ActionResult<Motorista> GetMotoristaById(int id)
         {
             //_context.Veiculos.FirstOrDefault(v => v.Id == id);
-            var motorista = _context.Motoristas.Find(id);
+            var motorista = _context.Motoristas.Include(m => m.Veiculo).FirstOrDefault(v => v.Id == id);
 
             if (motorista == null)
                 return NotFound();
@@ -35,7 +36,7 @@ namespace EstacionamentoSenac.API.Controllers
         }
 
         [HttpPost]
-        public ActionResult<Veiculo> PostMotorista(Motorista motorista)
+        public ActionResult<Motorista> PostMotorista(Motorista motorista)
         {
             _context.Motoristas.Add(motorista);
             _context.SaveChanges();
